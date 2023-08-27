051bf9f420488b570ca7b56319517da2f2276050
kushagra-raii <kushagrarai83@gmail.com>
2023-08-27 23:12:55 +0530

    Added OTP functionality.
    installed otp-generator library.
    created a controller for generating and sending otp while signUp.

---
+++ Server/controllers/Auth.js
@@ -0,0 +1,55 @@
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const otpGenerator = require("otp-generator");
+
+
+exports.sendOTP = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const checkUserPresent = await User.findOne({ email });
+
+    if (checkUserPresent) {
+      return res.json({
+        success: false,
+        message: "User already registered",
+      });
+    }
+
+    var otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+    });
+
+    let result = await OTP.findOne({ otp: otp });
+
+    while (result) {
+      otp = otpGenerator.generate(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      });
+
+      result = await OTP.findOne({ otp: otp });
+    }
+
+    const otpPayload = { email, otp };
+
+    const otpBody = await OTP.create(otpPayload);
+
+    res.json({
+      success: true,
+      message: "otp sent successfully",
+      otp,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error,
+    });
+  }
+};
+
+
+
