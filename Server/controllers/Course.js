2e7294d3952bcfd7a0e3fe80fe8f7e5d06404ea9
kushagra-raii <kushagrarai83@gmail.com>
2023-08-31 16:50:19 +0530

    Added Course controllers
    Instructor can create course.
    User can see all the courses.

---
+++ Server/controllers/Course.js
@@ -0,0 +1,109 @@
+const Course = require("../models/Course");
+const Category = require("../models/Category");
+const User = require("../models/User");
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+require("dotenv").config();
+
+exports.createCourse = async (req, res) => {
+  try {
+    const { courseName, courseDescription, whatYouWillLearn, price, category } =
+      req.body;
+    const thumbnail = req.files.thumbnailImage;
+
+    if (
+      !courseName ||
+      !courseDescription ||
+      !whatYouWillLearn ||
+      !price ||
+      !category
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are mandatory",
+      });
+    }
+
+    const userId = req.user.id;
+    const instructorDetails = await User.findById(userId);
+
+    if (!instructorDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Instructor not found",
+      });
+    }
+
+    const categoryDetails = await Category.findById(category);
+    if (!categoryDetails) {
+      return res.status(404).json({
+        success: false,
+        message: "Category details not found",
+      });
+    }
+
+    const thumbnailImage = await uploadImageToCloudinary(
+      thumbnail,
+      process.env.Folder_Name
+    );
+
+    const newCourse = await Course.create({
+      courseName,
+      courseDescription,
+      instructor: instructorDetails,
+      whatYouWillLearn: whatYouWillLearn,
+      price,
+      category: categoryDetails._id,
+      thumbnail: thumbnailImage.secure_url,
+    });
+
+    await User.findByIdAndUpdate(
+      { _id: instructorDetails._id },
+      {
+        $push: {
+          courses: newCourse._id,
+        },
+      },
+      { new: true }
+    );
+
+    //TODO: update category schema
+
+    return res.status(200).json({
+      success: true,
+      message: "Course created",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.showAllCourses = async (req, res) => {
+  try {
+    const allCourses = await Course.find(
+      {},
+      {
+        courseName: true,
+        price: true,
+        thumbnail: true,
+        instructor: true,
+        ratingAndReview: true,
+        studentsEnrolled: true,
+      }
+    )
+      .populate("instructor")
+      .exec();
+    return res.status(200).json({
+      success: true,
+      message: "All courses data fetched successfully",
+      data: allCourses,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
