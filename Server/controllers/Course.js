fa5f4cce339934b8b430264ce5faa960ec57565a
kushagra-raii <kushagrarai83@gmail.com>
2023-09-05 23:32:45 +0530

    Added functionality for course details
    User can get all the details of that particular course with id.

--- Server/controllers/Course.js
+++ Server/controllers/Course.js
@@ -107,3 +107,43 @@ exports.showAllCourses = async (req, res) => {
     });
   }
 };
+
+exports.getCourseDetails = async (req, res) => {
+  try {
+    const { courseId } = req.body;
+    const courseDetails = await Course.find({ _id: courseId })
+      .populate({
+        path: "instructor",
+        populate: {
+          path: "additionalDetails",
+        },
+      })
+      .populate("category")
+      .populate("ratingAndReviews")
+      .populate({
+        path: "courseContent",
+        populate: {
+          path: "subSection",
+        },
+      })
+      .exec();
+
+      if(!courseDetails){
+        return res.status(400).json({
+          success: false,
+          message: `Could not find the course with ${courseId}`,
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        message: "Course Details fetched successfully",
+        data:courseDetails,
+      });
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
